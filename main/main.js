709649642a3e09ba2664035020acb3b6f4bb5daf
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-06 22:45:57 +0800

    feature: return empty array with input of empty string

--- main/main.js
+++ main/main.js
@@ -1,28 +1,4 @@
-module.exports = function compare(answer, input) {
+module.exports = function generateAnagrams(input) {
 
-    let count = answer.split('').reduce((acc, cur, index) => {
-        if (input.includes(cur)) {
-            if (cur === input.charAt(index)) {
-                acc.A++;
-            } else {
-                acc.B++;
-            }
-        }
-        return acc;
-    }, {A: 0, B: 0});
-
-    return `${count.A}A${count.B}B`;
+    return [];
 };
-
-function generateAnswer() {
-    let answer = [];
-
-    while (answer.length < 4) {
-        let random = Math.floor(Math.random() * 10);
-        if (answer.includes(random)) {
-            continue;
-        }
-        answer.push(random);
-    }
-    return answer.join();
-}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,67 +1,14 @@
 const main = require('../main/main');
 
-describe('compare()', () => {
+describe('generateAnagrams()', () => {
 
-    var answer;
-
-    beforeEach(function () {
-        answer = "1234"
-    });
-
-    it('should return 4A0B when user input matches answer', function() {
-        let input = '1234';
-        let result = main(answer, input);
-        let expected = '4A0B';
+    it('should return [] when user inputs empty string', function() {
+        let input = '';
+        let result = main(input);
+        let expected = [];
         expect(result).toEqual(expected)
     });
 
-    it('should return 0A0B when user input does not match answer at all', function() {
-        let input = '5678';
-        let result = main(answer, input);
-        let expected = '0A0B';
-        expect(result).toEqual(expected)
-    });
 
-    it('should return 1A0B when user input matches one digit of answer but not others', function() {
-        let input = '1567';
-        let result = main(answer, input);
-        let expected = '1A0B';
-        expect(result).toEqual(expected)
-    });
-
-    it('should return 0A1B when user input matches one digit but not in the right position', function() {
-        let input = '5671';
-        let result = main(answer, input);
-        let expected = '0A1B';
-        expect(result).toEqual(expected)
-    });
-
-    it('should return 3A0B when user input matches three digits of answer but not others', function() {
-        let input = '1237';
-        let result = main(answer, input);
-        let expected = '3A0B';
-        expect(result).toEqual(expected)
-    });
-
-    it('should return 0A4B when user input matches all digits of answer but not in the right position', function() {
-        let input = '4321';
-        let result = main(answer, input);
-        let expected = '0A4B';
-        expect(result).toEqual(expected)
-    });
-
-    it('should return 0A4B when user input matches all digits of answer but not in the right position', function() {
-        let input = '4321';
-        let result = main(answer, input);
-        let expected = '0A4B';
-        expect(result).toEqual(expected)
-    });
-
-    it('should return 1A0B when user input same number and matches one digits of answer', function() {
-        let input = '1111';
-        let result = main(answer, input);
-        let expected = '1A0B';
-        expect(result).toEqual(expected)
-    });
 });
 
